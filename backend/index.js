0d657a053f1486cef5ff741d946e635f0b0b1d58
certicky <certicky@gmail.com>
2022-02-28 22:07:58 +0100

    backend: allow cross-origin reqs

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,11 @@
 const express = require('express')
+const cors = require('cors')
 const bodyParser = require('body-parser')
-const app = express()
 const reports = require('./api/reports')
 const { listenPort } = require('./settings')
 
+const app = express()
+app.use(cors())
 app.use(bodyParser.json())
 app.use(
   bodyParser.urlencoded({
